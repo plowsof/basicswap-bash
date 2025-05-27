5fb2016f391a317c59b155955885e1c2be5bbe4d
plowsof <plowsof@protonmail.com>
2025-05-27 18:52:38 +0100

    debug install

--- basicswap-install.sh
+++ basicswap-install.sh
@@ -176,22 +176,29 @@ read -p 'Press Enter to continue, or CTRL-C to exit.'
 addpath='PATH="$HOME/.local/bin:$PATH"'
 trasherdk=$(echo $PATH | grep -F '.local/bin')
 
+# $PATH in fedora git runner: 
+# /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 if [[ ! -d $HOME/.local/bin ]]; then
+    echo "DEBUG: $HOME/.local/bin dir does not exist"
     mkdir -p $HOME/.local/bin
 fi
 
 if [[ -z $trasherdk ]]; then
+    echo "DEBUG: inside trasherdk"
 
     # Bash
     if [[ -f $HOME/.bashrc ]] || [[ $DEBIAN ]]; then
+        echo "DEBUG: Bash"
         echo "export $addpath" | tee -a $HOME/.bashrc
     fi
     # Zsh
     if [[ -f $HOME/.zshrc ]]; then
+        echo "DEBUG: Zsh"
         echo "export $addpath" | tee -a $HOME/.zshrc
     fi
     # xfce4
     if [[ -f $HOME/.xsessionrc ]]; then
+        echo "DEBUG: xfce4"
         echo "export $addpath" | tee -a $HOME/.xsessionrc
     fi
 
